3fff3aa5ed841ce41422ec7ff82df90cc8a01f6b
SaeedAchiever <123863213+SaeedAchiever@users.noreply.github.com>
2023-07-25 14:51:51 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import React from 'react';
 import Navigation from './components/Navigation';
-//import Landingpage from './components/Landingpage';
+import Landingpage from './components/Landingpage';
 import HomeNote from './components/HomeNote';
-//import FirstClassRental from './components/FirstClassRental';
+import FirstClassRental from './components/FirstClassRental';
 import RentPage from './components/RentPage';
 import SmallNote from './components/SmallNote';
 import WhyUs from './components/WhyUs';
 import Contact from './components/Contact';
 import SaveMoney from './components/SaveMoney'
-//import WhoAreWe from './components/WhoAreWe';
+import WhoAreWe from './components/WhoAreWe';
 import Maps from './components/Maps';
-//import Testimonial from './components/Testimonial';
+import Testimonial from './components/Testimonial';
 import Footer from './components/Footer';
 import RentNow from './components/RentNow';
 
@@ -19,18 +19,18 @@ function App() {
   return (
     <div className="App">
        <Navigation />
-       {/*<Landingpage />*/}
+       <Landingpage />
        <HomeNote />
-       {/*<FirstClassRental />*/}
+       <FirstClassRental />
        <RentPage />
        <SmallNote />
        <RentNow />
        <WhyUs />
        <SaveMoney />
         <Contact />
-        {/*<WhoAreWe />*/}
+        <WhoAreWe />
         <Maps />
-        {/*<Testimonial />*/}
+        <Testimonial />
         <Footer />
     </div>
   );
